11d62d541e2866d3ca646573cbace394332cc31e
AndersonNS <andersonbruno.ns@gmail.com>
2017-06-13 21:09:06 -0300

    Trabalho DW
    Avaliação parte 3 de Desenvolvimento Web.

---
+++ js/jsa.js
@@ -0,0 +1,111 @@
+/* Smartphone */ /*Java para aparecer mensagem ao clicar no produto */
+function funcao1()
+{
+alert("Produto: Iphone 5s\nValor: R$ 1.500,00\nDisponível:24");
+}
+
+function funcao2()
+{
+alert("Produto: Iphone 7 plus\nValor: R$ R$ 4.049,10\nDisponível:15");
+}
+
+function funcao3()
+{
+alert("Produto: Galaxy S7 Edge\nValor: R$ R$ 2.339,90\nDisponível:20");
+}
+
+function funcao4()
+{
+alert("Produto: LG X Power\nValor: R$ 619,90\nDisponível:3");
+}
+
+function funcao5()
+{
+alert("Produto: Asus Zenfone 3\nValor: R$ 1.143,90\nDisponível:8");
+}
+
+/* Placa de Video */ /*Java para aparecer mensagem ao clicar no produto */
+function funcao6()
+{
+alert("Produto: Placa de Video GTX 1050\nValor: R$ 690,75\nDisponível:5");
+}
+
+function funcao7()
+{
+alert("Produto: Placa de Video GTX 1060\nValor: R$ 1.309,90\nDisponível:8");
+}
+
+function funcao8()
+{
+alert("Produto: Placa de Video GTX 1070\nValor: R$ 1.849,90\nDisponível:10");
+}
+
+function funcao9()
+{
+alert("Produto: Placa de Video RADEON 5450\nValor: R$ 113,90\nDisponível:30");
+}
+
+function funcao10()
+{
+alert("Produto: Placa de Video RX 570\nValor: R$ 929,90\nDisponível:1");
+}
+
+/* Monitor */ /*Java para aparecer mensagem ao clicar no produto */
+function funcao11()
+{
+alert("Produto: Monitor LED 19,5\nValor: R$ 432,90\nDisponível:2");
+}
+
+function funcao12()
+{
+alert("Produto: Monitor LED 25\nValor: R$ 808,90\nDisponível:4");
+}
+
+function funcao13()
+{
+alert("Produto: Monitor LED ASUS 24\nValor: R$ 900,75\nDisponível:0");
+}
+
+function funcao14()
+{
+alert("Produto: Monitor LED ASUS 27\nValor: R$ 2.644,90\nDisponível:9");
+}
+
+function funcao15()
+{
+alert("Produto: Monitor LED ASUS 28\nValor: R$ 929,90\nDisponível:18");
+}
+
+/* Mouse */ /*Java para aparecer mensagem ao clicar no produto */
+function funcao16()
+{
+alert("Produto: Mouse Gamer Centrophorus\nValor: R$ 65,90\nDisponível:14");
+}
+
+function funcao17()
+{
+alert("Produto: Mouse Gamer Deathadder\nValor: R$ 314,90\nDisponível:45");
+}
+
+function funcao18()
+{
+alert("Produto: Mouse Gamer Fortrek\nValor: R$ 36,90\nDisponível:32");
+}
+
+function funcao19()
+{
+alert("Produto: Mouse Gamer Prodigy\nValor: R$ 170,90\nDisponível:17");
+}
+
+function funcao20()
+{
+alert("Produto: Mouse Gamer Warrior\nValor: R$ 89,90\nDisponível:26");
+}
+
+/*Java para aparecer mensagem ao clicar na imagem X canto superior direito*/
+/* Compativel sem erros no navegador Chrome */
+function mensagem(){
+alert("Anderson Bruno RA:20171535\nGabriel Oliveira RA:20170064");
+var tab = window.open(window.location,"_top");
+tab.close();
+}
